fbbc3d06a128f7d0608348c6d252eb29aebf28ed
hojoon <ghwns107@naver.com>
2023-02-20 22:40:46 +0900

    pagiNation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Pagination from "./components/Pagination";
+
+interface Response {
+  totalPassengers: number;
+  totalPages: number;
+  data: Array<Passenger>;
+}
+interface Passenger {
+  _id: string;
+  name: string;
+  trips: number;
+  airline: Airline;
+  __v: number;
+}
+
+interface Airline {
+  id: number;
+  name: string;
+  country: string;
+  logo: string;
+  slogan: string;
+  head_quaters: string;
+  website: string;
+  established: string;
+}
 
 export default function App() {
-  return <div className="App">Hello Pagination</div>;
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [items, setItems] = useState<Array<Passenger>>([]);
+  const handlePageChange = (currentPage: number): void => {
+    setPage(currentPage);
+  };
+  useEffect(() => {
+    const fetch = async () => {
+      const params = { page, size: 10 };
+      const {
+        data: { totalPages, data },
+      } = await axios.get<Response>(
+        "https://api.instantwebtools.net/v1/passenger",
+        {
+          params,
+        }
+      );
+      setTotalPages(totalPages);
+      setItems(data);
+      console.log(items);
+      console.log(data);
+    };
+
+    fetch();
+  }, []);
+  return (
+    <div className="App">
+      <ul>
+        {items.map((item) => (
+          <li key={item._id}>{item.name}</li>
+        ))}
+      </ul>
+      <Pagination
+        count={totalPages}
+        page={page}
+        onPageChange={handlePageChange}
+      ></Pagination>
+    </div>
+  );
 }
---
+++ src/components/Pagination.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import styled from "@emotion/styled/macro";
+import usePagination from "../hooks/usePagination";
+import { GrFormPrevious, GrFormNext } from "react-icons/gr";
+import { AiOutlineEllipsis } from "react-icons/ai";
+
+interface Props {
+  count: number;
+  page: number;
+  onPageChange: (page: number) => void;
+  disabled?: boolean;
+  siblingCount?: number;
+  boundaryCount?: number;
+}
+
+const Navigation = styled.nav``;
+
+const Button = styled.button<{ selected?: boolean }>`
+  color: ${({ selected }) => (selected ? "#fff" : "#000")};
+  border: 0;
+  margin: 0;
+  padding: 8px 12px;
+  font-size: 16px;
+  font-weight: normal;
+  background-color: ${({ selected }) => (selected ? "#3d6afa" : "#fff")};
+  cursor: pointer;
+  border-radius: 100%;
+  width: 48px;
+  height: 48px;
+  &:hover {
+    background-color: #ccc;
+    color: #fff;
+  }
+  &:active {
+    opacity: 0.8;
+  }
+`;
+
+const Item = styled.li``;
+
+const ItemList = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  list-style: none;
+  ${Item} + ${Item} {
+    margin-left: 8px;
+  }
+`;
+
+const Pagination: React.FC<Props> = ({
+  count,
+  page,
+  onPageChange,
+  disabled,
+  siblingCount = 1,
+  boundaryCount = 1,
+}) => {
+  const getLabel = (item: number | string) => {
+    if (typeof item === "number") return item;
+    else if (item.indexOf("ellipsis") > -1)
+      return <AiOutlineEllipsis></AiOutlineEllipsis>;
+    else if (item.indexOf("prev") > -1)
+      return <GrFormPrevious></GrFormPrevious>;
+    else if (item.indexOf("next") > -1) return <GrFormNext></GrFormNext>;
+  };
+
+  const { items } = usePagination({
+    count,
+    page,
+    onPageChange,
+    disabled,
+    siblingCount,
+    boundaryCount,
+  });
+
+  return (
+    <Navigation>
+      <ItemList>
+        {items.map(({ key, disabled, selected, item, onClick }) => (
+          <Item key={key}>
+            <Button selected={selected} disabled={disabled} onClick={onClick}>
+              {getLabel(item)}
+            </Button>
+          </Item>
+        ))}
+      </ItemList>
+    </Navigation>
+  );
+};
+
+export default Pagination;
