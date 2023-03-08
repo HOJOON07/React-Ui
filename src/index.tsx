0cdfc2f0c001b7e2af49ae0ce5908dc6301f2321
hojoon <ghwns107@naver.com>
2023-03-08 10:10:17 +0900

    라우터 작업중

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import Router from "./Router";
 import App from "./App";
 import "./index.css";
 
@@ -9,11 +9,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <BrowserRouter>
-    <App></App>
-  </BrowserRouter>
-);
+root.render(<Router></Router>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
